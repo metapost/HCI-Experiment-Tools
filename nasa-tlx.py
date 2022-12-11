8187001743d2000a1e80f8828fa70ffa160068ed
Haipeng Wang <haipeng.wang@gmail.com>
2022-12-11 11:22:17 +0800

    feature: block

--- nasa-tlx.py
+++ nasa-tlx.py
@@ -1,14 +1,21 @@
+# Modified by hapeng.wang@gmail.com
+# v1.1, 20221211, added block, check file exists or not, added button of exit.
+# v1.0, 20221210
+
+# You MUST customize the "header" info before using the scale.
+
 #!/usr/bin/python3
 
 ## install: pip3 install appjar
+import os
 from appJar import gui
 
 ## Texts for the individual questionnaire items
 texts = ["Mental Demand    -    How mentally demanding was the task?",
          "Physical Demand    -    How physically demanding was the task?",
-         "Temporal Demand    -    How hurried or rushed was the pace of the task?",
-         "Performance    -    How successful were you in accomplishing what you were asked to do?",
-         "Effort    -    How hard did you have to work to accomplish your level of performance?",
+         # "Temporal Demand    -    How hurried or rushed was the pace of the task?",
+         # "Performance    -    How successful were you in accomplishing what you were asked to do?",
+         # "Effort    -    How hard did you have to work to accomplish your level of performance?",
          "Frustration    -    How insecure, discouraged, irritated, stressed and annoyed were you?"]
 
 ## Labels on the left and right sides of the scale
@@ -16,42 +23,61 @@ left_labels = ["Very Low", "Very Low", "Very Low", "Perfect", "Very Low", "Very 
 right_labels = ["Very High", "Very High", "Very High", "Failure", "Very High", "Very High"]
 
 ## Labels of the Conditions to be chosen from
-conditions = ["Condition 1", "Condition 2"]
+conditions = ["pop", "pull", "auto"]
 
 ## Experiments to be chosen from
-experiments = ["Experiment 1", "Experiment 2"]
+# experiments = ["Experiment 1", "Experiment 2"]
+experiments = ["2"]
+
+# block number
+blocks = ["0", "1", "2", "3"]
 
 
 ## Called when the submit button is clicked
 def on_submit():
+    if not os.path.exists("./nasa-rawtlx-results.txt"):
+        # define header of log file.
+        header = '# ' + 'experiment ' + 'user ' + 'condition ' + 'block ' + 'mental ' + 'physical ' + 'frustration '
+        file_handle = open("nasa-rawtlx-results.txt", "a")
+        file_handle.write(header + '\n')
+        file_handle.close()        
+        
     experiment = app.getOptionBox("Experiment")
     user_id = app.getSpinBox("User ID")
     condition = app.getOptionBox("Condition")
+    block = app.getOptionBox("Block")
+    
+    file_handle = open("nasa-rawtlx-results.txt", "a")
 
     write_string = ''
-    write_string += str(experiment) + ','
-    write_string += str(user_id) + ','
-    write_string += str(condition)
+    write_string += str(experiment) + ' '
+    write_string += str(user_id) + ' '
+    write_string += str(condition) + ' '
+    write_string += str(block)
 
     for i in range(len(texts)):
-        write_string += ',' + str(app.getScale("q" + str(i)) * 5)
+        write_string += ' ' + str(app.getScale("q" + str(i)) * 5)
 
-    file_handle = open("nasa-tlx-results.txt", "a")
     file_handle.write(write_string + '\n')
     file_handle.close()
+    
+    app.infoBox("RawTLX Input", "Input successfully.")
     print("The results were written successfully.")
+
+def on_exit():
     app.stop()
 
 
 ## Main entry point
 app = gui()
-app.setTitle("NASA-TLX")
+app.setTitle("NASA-RawTLX")
 app.setSize(1000, 700)
-app.setFont(10)
+app.setFont(size=16, weight="bold")
 
 app.addLabelOptionBox("Experiment", experiments, 0, 0)
-app.addLabelSpinBoxRange("User ID", 1, 200, 0, 1)
-app.addLabelOptionBox("Condition", conditions, 0, 2)
+app.addLabelSpinBoxRange("User ID", 1, 100, 0, 1)
+app.addLabelOptionBox("Block", blocks, 0, 2)
+app.addLabelOptionBox("Condition", conditions, 0, 3)
 app.addHorizontalSeparator(2, 0, 3)
 
 for i, entry in enumerate(texts):
@@ -67,6 +93,8 @@ for i, entry in enumerate(texts):
 
     app.setScaleRange("q" + str(i), 0, 20, 10)
     app.setScaleIncrement("q" + str(i), 1)
+    app.showScaleIntervals("q" + str(i), 1)
+    app.showScaleValue("q" + str(i), show=True)
 
     app.setSticky("we")
     app.addLabel("ticks_q" + str(i), "||", 4*i + 2 + 3, 1)
@@ -74,5 +102,6 @@ for i, entry in enumerate(texts):
 
 app.setSticky("we")
 app.addButton("Submit", on_submit, 4*len(texts) + 1 + 3, 1)
+app.addButton("Exit", on_exit, 4*len(texts) + 1 + 3, 3)
 
 app.go()
